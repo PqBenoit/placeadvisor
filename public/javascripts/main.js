20e604f2ae374d91bbc00c4b86caee53b5bc65a6
PqBenoit <benoit@monsieurdrive.com>
2015-02-20 15:50:45 +0100

    Reviens list, review show, review edit, review deletion, all wrapped in front

---
+++ public/javascripts/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$('.alert-info').hide();
+
+	var xmlhttp; 
+
+	$('.remove-button').on('click', function(){
+
+		xmlhttp = new XMLHttpRequest();
+		xmlhttp.open('DELETE', 'http://localhost:3000/reviews/' + $(this).attr('data-id'), true);
+		xmlhttp.send(null);
+
+	});
+
+	$('.edit-button').on('click', function(e){
+		
+		e.preventDefault();
+
+		var $inputs = $('.form-edition :input');
+
+		var data = [];
+		$inputs.each(function(index) {
+			data.push($(this).val());
+		});
+
+		$.ajax({
+			url: "http://localhost:3000/reviews/" + $(this).attr('data-id'),
+			data: {
+				name: data[0],
+				placeType: data[1],
+				stars: data[2]
+			},
+			type: 'PUT',
+			contentType: "application/json",
+			dataType: 'json',
+			success: function(response){
+				$('.alert-info').fadeIn(300);
+			},
+			error: function(error){
+
+			}
+		});
+	});
+});
