27d3098369bbd6038ef0a6a81af43f24a7d87a46
PqBenoit <benoit@monsieurdrive.com>
2015-02-20 18:09:41 +0100

    Search working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,6 +10,42 @@ $(document).ready(function(){
 		xmlhttp.send(null);
 
 		location.reload();
+	});
+
+	$('.add-button').on('click', function(e){
+		e.preventDefault();
+
+		var $inputs = $('.form-creation :input');
+
+		var data = [];
+		$inputs.each(function(index) {
+			if(!$(this).val())
+				$(this).css({
+					border: "solid 1px #fab4b4"
+				});
+			else
+				$(this).css({
+					border: "solid 1px #ccc"
+				});
+				data.push($(this).val());
+		});
+
+		$.ajax({
+			url: "http://localhost:3000/reviews/",
+			data: {
+				name: data[0],
+				placeType: data[1],
+				stars: data[2]
+			},
+			type: 'POST',
+			dataType: 'json',
+			success: function(response){
+				$('.alert-info').fadeIn(300);
+			},
+			error: function(error){
+
+			}
+		});
 
 	});
 
@@ -32,6 +68,35 @@ $(document).ready(function(){
 				stars: data[2]
 			},
 			type: 'PUT',
+			dataType: 'json',
+			success: function(response){
+				$('.alert-info').fadeIn(300);
+			},
+			error: function(error){
+
+			}
+		});
+	});
+
+	$('.search-button').on('click', function(e){
+		
+		e.preventDefault();
+
+		var $inputs = $('.form-edition :input');
+
+		var data = [];
+		$inputs.each(function(index) {
+			data.push($(this).val());
+		});
+
+		$.ajax({
+			url: "http://localhost:3000/reviews/search",
+			data: {
+				name: data[0],
+				placeType: data[1],
+				stars: data[2]
+			},
+			type: 'GET',
 			dataType: 'json',
 			success: function(response){
 				$('.alert-info').fadeIn(300);
