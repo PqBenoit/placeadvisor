639d9977d45a5ac47865b9eee8e8f7f539826c4e
Joan Ortega <jomaora@gmail.com>
2015-03-20 17:11:26 +0100

    rewriting 27d3098369bbd6038ef0a6a81af43f24a7d87a46

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,6 +9,44 @@ $(document).ready(function(){
 		xmlhttp.open('DELETE', 'http://localhost:3000/reviews/' + $(this).attr('data-id'), true);
 		xmlhttp.send(null);
 
+		location.reload();
+	});
+
+	$('.add-button').on('click', function(e){
+		e.preventDefault();
+
+		var $inputs = $('.form-creation :input');
+
+		var data = [];
+		$inputs.each(function(index) {
+			if(!$(this).val())
+				$(this).css({
+					border: "solid 1px #fab4b4"
+				});
+			else
+				$(this).css({
+					border: "solid 1px #ccc"
+				});
+				data.push($(this).val());
+		});
+
+		$.ajax({
+			url: "http://localhost:3000/reviews/",
+			data: {
+				name: data[0],
+				placeType: data[1],
+				stars: data[2]
+			},
+			type: 'POST',
+			dataType: 'json',
+			success: function(response){
+				$('.alert-info').fadeIn(300);
+			},
+			error: function(error){
+
+			}
+		});
+
 	});
 
 	$('.edit-button').on('click', function(e){
@@ -30,7 +68,35 @@ $(document).ready(function(){
 				stars: data[2]
 			},
 			type: 'PUT',
-			contentType: "application/json",
+			dataType: 'json',
+			success: function(response){
+				$('.alert-info').fadeIn(300);
+			},
+			error: function(error){
+
+			}
+		});
+	});
+
+	$('.search-button').on('click', function(e){
+		
+		e.preventDefault();
+
+		var $inputs = $('.form-edition :input');
+
+		var data = [];
+		$inputs.each(function(index) {
+			data.push($(this).val());
+		});
+
+		$.ajax({
+			url: "http://localhost:3000/reviews/search",
+			data: {
+				name: data[0],
+				placeType: data[1],
+				stars: data[2]
+			},
+			type: 'GET',
 			dataType: 'json',
 			success: function(response){
 				$('.alert-info').fadeIn(300);
--- routes/reviews.js
+++ routes/reviews.js
@@ -58,6 +58,128 @@ router.delete('/', function(req, res){
 	});
 });
 
+router.get('/top', function(req, res){
+
+	var accept = accepts(req);
+
+	Review.find().sort({stars: -1}).limit(3).exec(function(err, reviews) {
+		if(err) {
+			res.send(404, err);
+		}
+		else {
+			if(accept.type(['json', 'html']) == 'json'){
+				res.setHeader('Content-Type', 'application/json');
+				res.send(200, reviews);
+			}
+			else if(accept.type(['json', 'html']) == 'html') {
+				res.setHeader('Content-Type', 'text/html')
+				res.render('reviews/top', { reviews: reviews });
+			}
+		}
+	});
+});
+
+router.get('/new', function(req, res){
+	res.render('reviews/new');
+});
+
+router.get('/search', function(req, res){
+
+	var accept = accepts(req);
+
+	if(req.query.name){
+		Review.find({name: req.query.name}, function(err, reviews){
+			if(err) {
+				res.send(404, err);
+			}
+			else {
+				if(accept.type(['json', 'html']) == 'json'){
+					res.setHeader('Content-Type', 'application/json');
+					res.send(200, reviews);
+				}
+				else if(accept.type(['json', 'html']) == 'html') {
+					console.log(reviews);
+					res.setHeader('Content-Type', 'text/html');
+					res.render('reviews/search', { reviews: reviews });
+				}
+			}
+		});
+	} else if (req.query.placeType){
+		Review.find({placeType: req.query.placeType}, function(err, reviews){
+			if(err) {
+				res.send(404, err);
+			}
+			else {
+				if(accept.type(['json', 'html']) == 'json'){
+					res.setHeader('Content-Type', 'application/json');
+					res.send(200, reviews);
+				}
+				else if(accept.type(['json', 'html']) == 'html') {
+					console.log(reviews);
+					res.setHeader('Content-Type', 'text/html');
+					res.render('reviews/search', { reviews: reviews });
+				}
+			}
+		});
+	} else if (req.query.stars){
+		Review.find({stars: req.query.stars}, function(err, reviews){
+			if(err) {
+				res.send(404, err);
+			}
+			else {
+				if(accept.type(['json', 'html']) == 'json'){
+					res.setHeader('Content-Type', 'application/json');
+					res.send(200, reviews);
+				}
+				else if(accept.type(['json', 'html']) == 'html') {
+					console.log(reviews);
+					res.setHeader('Content-Type', 'text/html');
+					res.render('reviews/search', { reviews: reviews });
+				}
+			}
+		});
+	} else {
+		res.render('reviews/search');
+	}
+	
+		// Review.find({placeType: req.query.placeType}, function(err, reviews){
+		// 	if(err) {
+		// 		res.send(404, err);
+		// 	}
+		// 	else {
+		// 		if(accept.type(['json', 'html']) == 'json'){
+		// 			res.setHeader('Content-Type', 'application/json');
+		// 			res.send(200, reviews);
+		// 		}
+		// 		else if(accept.type(['json', 'html']) == 'html') {
+		// 			console.log(req.query);
+		// 			res.setHeader('Content-Type', 'text/html');
+		// 			res.render('reviews/search', { reviews: reviews });
+		// 		}
+		// 	}
+		// });
+	
+
+	// if(req.query.placeType) {
+	// 	Review.find({placeType: req.query.placeType}, function(err, reviews){
+	// 		if(err) {
+	// 			res.send(404, err);
+	// 		}
+	// 		else {
+	// 			if(accept.type(['json', 'html']) == 'json'){
+	// 				res.setHeader('Content-Type', 'application/json');
+	// 				res.send(200, reviews);
+	// 			}
+	// 			else if(accept.type(['json', 'html']) == 'html') {
+	// 				console.log(req.query);
+	// 				res.setHeader('Content-Type', 'text/html');
+	// 				res.render('reviews/search', { reviews: reviews });
+	// 			}
+	// 		}
+	// 	});
+	// }
+});
+
 
 ///////////////////////////////
 ////// Routes WITH id /////////
@@ -79,6 +201,25 @@ router.get('/:id', function(req, res){
 			else if(accept.type(['json', 'html']) == 'html') {
 				res.setHeader('Content-Type', 'text/html');
 				res.render('reviews/show', { review: review });
+			}
+	});
+});
+
+router.get('/edit/:id', function(req, res){
+
+	var accept = accepts(req);
+
+	Review.findById(req.params.id, function(err, review){
+		if(err)
+			res.send(404, err);
+		else
+			if(accept.type(['json', 'html']) == 'json'){
+				res.setHeader('Content-Type', 'application/json');
+				res.send(200, review);
+			}
+			else if(accept.type(['json', 'html']) == 'html') {
+				res.setHeader('Content-Type', 'text/html');
+				res.render('reviews/edit', { review: review });
 			}
 	});
 });
