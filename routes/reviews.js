214f389c7c2cdcffb308bd21b80b257bec45f99f
Joan Ortega <jomaora@gmail.com>
2015-03-20 17:10:03 +0100

    rewriting 842c35d0b488eb7a7ef2d907d48783ca6b47b17a

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var lodash = require('lodash');
 
+var accepts = require('accepts'); //Used to check accept's format
+
 var Review = require('../models/review');
 
 
@@ -11,47 +13,49 @@ var Review = require('../models/review');
 
 
 router.get('/', function(req, res){
+
+	var accept = accepts(req);
+
 	Review.find( {} , function(err, reviews){
 		if(err) {
 			res.send(404, err);
 		}
 		else {
-			res.send(200, reviews);
+			if(accept.type(['json', 'html']) == 'json'){
+				res.setHeader('Content-Type', 'application/json');
+				res.send(200, reviews);
+			}
+			else if(accept.type(['json', 'html']) == 'html') {
+				res.setHeader('Content-Type', 'text/html')
+				res.render('reviews/index', { reviews: reviews });
+			}
 		}
 	});
 });
 
 router.post('/', function(req, res){
-	if(req.isAuthenticated()){
-		review = new Review();
-		review.name = req.body.name;
-		review.placeType = req.body.placeType;
-		review.stars = req.body.stars;
+	review = new Review();
+	review.name = req.body.name;
+	review.placeType = req.body.placeType;
+	review.stars = req.body.stars;
 
-		review.save(function(err, review){
-			if(err){
-				res.send(400, err);
-			}
-			else {
-				res.send(201, review);
-			}
-		});
-	}
-	else
-		res.json({message: 'You have to be authenticated to add a review'});
+	review.save(function(err, review){
+		if(err){
+			res.send(400, err);
+		}
+		else {
+			res.send(201, review);
+		}
+	});
 });
 
 router.delete('/', function(req, res){
-	if(req.isAuthenticated()){
-		Review.remove(function(err){
-			if(err)
-				res.send(404, err);
-			else
-				res.send(200, {message: 'Reviews deleted.'});
-		});
-	}
-	else
-		res.send({message: "You have to be authenticated to delete reviews"});
+	Review.remove(function(err){
+		if(err)
+			res.send(404, err);
+		else
+			res.send(200, {message: 'Reviews deleted.'});
+	});
 });
 
 
@@ -62,43 +66,43 @@ router.delete('/', function(req, res){
 
 router.get('/:id', function(req, res){
 
+	var accept = accepts(req);
+
 	Review.findById(req.params.id, function(err, review){
 		if(err)
 			res.send(404, err);
 		else
-			res.send(200, review);
+			if(accept.type(['json', 'html']) == 'json'){
+				res.setHeader('Content-Type', 'application/json');
+				res.send(200, review);
+			}
+			else if(accept.type(['json', 'html']) == 'html') {
+				res.setHeader('Content-Type', 'text/html');
+				res.render('reviews/show', { review: review });
+			}
 	});
 });
 
 router.put('/:id', function(req, res){
 
-	if(req.isAuthenticated()){
-		Review.update({ _id: req.params.id }, req.body, function(err, review){
-			if(err) {
-				res.send(404, err);
-			}
-			else {
-				res.send(200, {message: 'Review modified'});
-			}
-		});
-	}
-	else
-		res.json({message: 'You have to be authenticated to modify a review'});
-
+	Review.update({ _id: req.params.id }, req.body, function(err, review){
+		if(err) {
+			res.send(404, err);
+		}
+		else {
+			res.send(200, {message: 'Review modified'});
+		}
+	});
 });
 
 router.delete('/:id', function(req, res){
 	
-	if(req.isAuthenticated()){
-		Review.remove({ _id: req.params.id }, function(err, review){
-			if(err)
-				res.send(404, err);
-			else
-				res.send(200, { message: 'Review deleted'});
-		});
-	}
-	else
-		res.send({message: "You have to be authenticated to delete reviews"});
+	Review.remove({ _id: req.params.id }, function(err, review){
+		if(err)
+			res.send(404, err);
+		else
+			res.send(200, { message: 'Review deleted'});
+	});
 });
 
 module.exports = router;
